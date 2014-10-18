82ebf2f03cbac478c58a79e1991f99518a1060f7
Godefroid Chapelle <gotcha@bubblenet.be>
2014-10-18 11:29:53 +0200

    ssh key

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ SOFTWARE_DOWNLOAD=~/auto/software
 #configure git
 git config --global user.name "Godefroid Chapelle"
 git config --global user.email gotcha@bubblenet.be
+
+#ssh key
+if [ ! -f /Users/gotcha/.ssh/id_dsa ]
+then
+    ssh-keygen -q -t dsa -f /Users/gotcha/.ssh/id_dsa -N ""
+fi
